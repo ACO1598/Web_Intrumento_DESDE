52ff32aa97dacac62d8a63ffee1cfd457c124d0b
Alan Clech <47863396+ACO1598@users.noreply.github.com>
2022-05-03 00:50:58 +0200

    Testing Listado principal
    home_test.component.html es para realizar pruebas

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Account } from 'app/core/auth/account.model';
 
 @Component({
   selector: 'jhi-home',
-  templateUrl: './home.component.html',
+  templateUrl: './home_test.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
