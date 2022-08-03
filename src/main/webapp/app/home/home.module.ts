9c3a1acf0c8ee8cebec0e649780d46ffd5227dbe
Alan Clech <47863396+ACO1598@users.noreply.github.com>
2022-08-03 11:47:52 +0200

    Cambios en las entidades para acomodar el tipo de dato, post cambio DB

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { HomeListComponent } from './home-list/home-list.component';
+import { HomeElementComponent } from './home-element/home-element.component';
 
 @NgModule({
   imports: [SharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeListComponent, HomeElementComponent],
 })
 export class HomeModule {}
